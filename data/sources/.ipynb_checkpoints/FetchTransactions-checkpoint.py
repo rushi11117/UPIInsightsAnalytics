81e6f7279fed3161c6df1ec15609726406078e8a
Rushikesh Mhetre <79018314+rushi11117@users.noreply.github.com>
2023-12-30 16:55:28 +0530

    30 Dec 2023

---
+++ data/sources/.ipynb_checkpoints/FetchTransactions-checkpoint.py
@@ -0,0 +1,58 @@
+#### Imports Requered for Scraping or Access microsevice endpoint
+
+# ! pip install requests
+# ! pip install pandas
+# ! pip install pymongo
+# ! pip install random
+# ! pip install collections
+# ! pip install configparser
+
+
+import requests
+import pandas as pd
+from pymongo import MongoClient
+import random
+import configparser
+
+# help(requests)
+class FetchTransactions:
+    def getTransactionsFromGateway(self):  
+        try:
+            ResponseEntity = requests.get('http://localhost:8085/transactions-insights')
+            ResponseEntity.raise_for_status()  # Raise an error for HTTP errors
+        except requests.HTTPError as e:
+            print(e)
+            return pd.DataFrame()  # Return an empty DataFrame on error
+        return pd.DataFrame(ResponseEntity.json())
+
+class FetchLocations:
+    def getAllLocationsCoordinates(self):
+        config = configparser.ConfigParser()
+        config.read(r'D:\Data Analysis\upi-data-analytics\databases\mongoDB\mongodb_config.ini')
+        host = config['MongoDB']['host']
+        port = config['MongoDB']['port']
+        database = config['MongoDB']['database']
+        
+        client = MongoClient(host, int(port))
+        db = client[database]
+        collection = db['coordinates']  # Corrected the collection name
+        return list(collection.find({}, {'_id': 0}))
+
+class FetchTransactionsWithLocations:
+    def __init__(self):
+        self.transaction_gateway = FetchTransactions()
+        self.location_fetcher = FetchLocations()
+
+    def getAllTransactionsWithLocations(self):
+        transactions_df = self.transaction_gateway.getTransactionsFromGateway()
+        locations = self.location_fetcher.getAllLocationsCoordinates()
+        print(locations)
+        print(len(transactions_df))
+        if not transactions_df.empty:
+            no_of_transactions = len(transactions_df.keys())
+            transactions_df['TRANSACTION_CITY'] = [random.choice(locationst)[str(i % 99)] for i in range(no_of_transactions)]
+        return transactions_df  # Return the modified DataFrame
+        
+transactions_with_locations = FetchTransactionsWithLocations()
+result = transactions_with_locations.getAllTransactionsWithLocations()
+print(result)
